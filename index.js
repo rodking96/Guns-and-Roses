ce2a3fbc99c556b382bdf58c0d6160acab953abf
Rodrigo Reyes <rodrigoreyes96@gmail.com>
2022-01-16 09:56:18 -0500

    sixth commit

--- index.js
+++ index.js
@@ -18,24 +18,24 @@ window.addEventListener("keydown", (e) => {
     villain.appendChild(laser);
 
     var movelaser = setInterval(() => {
-      var guns = document.getElementsByClassName("guns");
+      var roses = document.getElementsByClassName("roses");
 
-      for (var i = 0; i < guns.length; i++) {
-        var gun = guns[i];
-        if (gun != undefined) {
-          var gunbound = gun.getBoundingClientRect();
+      for (var i = 0; i < roses.length; i++) {
+        var rose = roses[i];
+        if (rose != undefined) {
+          var rosebound = rose.getBoundingClientRect();
           var laserbound = laser.getBoundingClientRect();
 
-          //Check whether the gun and the laser are at the same position
-          //If so,destroy the guns
+          //Check whether the rose and the laser are at the same position
+          //If so,destroy the roses
 
           if (
-            laserbound.left >= gunbound.left &&
-            laserbound.right <= gunbound.right &&
-            laserbound.top <= gunbound.top &&
-            laserbound.bottom <= gunbound.bottom
+            laserbound.left >= rosebound.left &&
+            laserbound.right <= rosebound.right &&
+            laserbound.top <= rosebound.top &&
+            laserbound.bottom <= rosebound.bottom
           ) {
-            gun.parentElement.removeChild(gun); //Just removing that particular gun;
+            rose.parentElement.removeChild(rose); //Just removing that particular rose;
             //Score
             document.getElementById("points").innerHTML =
               parseInt(document.getElementById("points").innerHTML) + 1;
@@ -57,37 +57,37 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
-var generateguns = setInterval(() => {
-  var gun = document.createElement("div");
-  gun.classList.add("guns");
-  //Just getting the left of the gun to place it in random position...
-  var gunleft = parseInt(
-    window.getComputedStyle(gun).getPropertyValue("left")
+var generateroses = setInterval(() => {
+  var rose = document.createElement("div");
+  rose.classList.add("roses");
+  //Just getting the left of the rose to place it in random position...
+  var roseleft = parseInt(
+    window.getComputedStyle(rose).getPropertyValue("left")
   );
-  //generate value between 0 to 450 where 450 => villain width - gun width
-  gun.style.left = Math.floor(Math.random() * 450) + "px";
+  //generate value between 0 to 450 where 450 => villain width - rose width
+  rose.style.left = Math.floor(Math.random() * 450) + "px";
 
-  villain.appendChild(gun);
+  villain.appendChild(rose);
 }, 1000);
 
-var moveguns = setInterval(() => {
-  var guns = document.getElementsByClassName("guns");
+var moveroses = setInterval(() => {
+  var roses = document.getElementsByClassName("roses");
 
-  if (guns != undefined) {
-    for (var i = 0; i < guns.length; i++) {
-      //Now I have to increase the top of each gun,so that the guns can move downwards
-      var gun = guns[i]; //getting each gun
-      var guntop = parseInt(
-        window.getComputedStyle(gun).getPropertyValue("top")
+  if (roses != undefined) {
+    for (var i = 0; i < roses.length; i++) {
+      //Now I have to increase the top of each rose,so that the roses can move downwards
+      var rose = roses[i]; //getting each rose
+      var rosetop = parseInt(
+        window.getComputedStyle(rose).getPropertyValue("top")
       );
-      //475 => villainheight - gunheight + 25
-      if (guntop >= 475) {
+      //475 => villainheight - roseheight + 25
+      if (rosetop >= 475) {
         alert("Game Over");
-        clearInterval(moveguns);
+        clearInterval(moveroses);
         window.location.reload();
       }
 
-      gun.style.top = guntop + 25 + "px";
+      rose.style.top = rosetop + 25 + "px";
     }
   }
 }, 450);
